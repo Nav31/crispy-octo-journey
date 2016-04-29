29faeae944ee8f68487e49fa5466108ffc5a5ee9
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-29 12:39:34 -0400

    moved where the element appears on the page

--- content.js
+++ content.js
@@ -11,8 +11,8 @@ $(document).ready(function(){
 			var x = event.clientX;
 			var y = event.clientY;
 			commentElement.style.position = 'absolute';
-			commentElement.style.left = x+'px';
-			commentElement.style.top = y+'px';	
+			commentElement.style.left = x +'px';
+			commentElement.style.top = (y - 30) +'px';	
 			commentElement.style.zIndex = "9999";
 			document.body.appendChild(commentElement);	
 		});
