4de8564470f285059a5ee2d2a3be3bdd1f91ccad
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-29 14:52:08 -0400

    became a button

--- content.js
+++ content.js
@@ -5,16 +5,16 @@ $(document).ready(function(){
 	chrome.storage.onChanged.addListener(function(value){
 
 		document.addEventListener('click', function(event){
-			var commentElement = document.createElement('p');
+			var theButton = document.createElement('button');
 			var text = document.createTextNode(value.value.newValue);
-			commentElement.appendChild(text);
+			theButton.appendChild(text);
 			var x = event.clientX;
 			var y = event.clientY;
-			commentElement.style.position = 'absolute';
-			commentElement.style.left = x +'px';
-			commentElement.style.top = (y - 30) +'px';	
-			commentElement.style.zIndex = "9999";
-			document.body.appendChild(commentElement);	
+			theButton.style.position = 'absolute';
+			theButton.style.left = x +'px';
+			theButton.style.top = (y - 30) +'px';	
+			theButton.style.zIndex = "9999";
+			document.body.appendChild(theButton);	
 		});
 	});
 });
