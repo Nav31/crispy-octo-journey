da45423354d950d5d5a48687676fc9ae59c6b5c4
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-29 15:03:16 -0400

    now attaches one button

--- content.js
+++ content.js
@@ -5,14 +5,17 @@ $(document).ready(function(){
 	chrome.storage.onChanged.addListener(function(value){
 
 		document.addEventListener('click', function(event){
+			if(document.getElementById('whatever')) return;
 			var theButton = document.createElement('button');
+			theButton.setAttribute('id', 'whatever');
 			var text = document.createTextNode(value.value.newValue);
+			console.log(theButton);
 			theButton.appendChild(text);
 			var x = event.clientX;
 			var y = event.clientY;
 			theButton.style.position = 'absolute';
-			theButton.style.left = x +'px';
-			theButton.style.top = (y - 30) +'px';	
+			theButton.style.left = (x - 20) +'px';
+			theButton.style.top = (y - 20) +'px';	
 			theButton.style.zIndex = "9999";
 			document.body.appendChild(theButton);	
 		});
