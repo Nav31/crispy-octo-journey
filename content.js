edc1e5bef1cdc037c2ad5a250391161485b5ddea
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-04-29 12:14:46 -0400

    removed consolelogs

--- content.js
+++ content.js
@@ -4,19 +4,16 @@ $(document).ready(function(){
 
 	chrome.storage.onChanged.addListener(function(value){
 		var commentElement = document.createElement('p');
-		console.log(value);
 		var text = document.createTextNode(value.value.newValue);
 		commentElement.appendChild(text);
 
 		document.addEventListener('click', function(event){
 			var x = event.clientX;
 			var y = event.clientY;
-			console.log(x + y)
 			commentElement.style.position = 'absolute';
 			commentElement.style.left = x+'px';
 			commentElement.style.top = y+'px';	
 			commentElement.style.zIndex = "9999";
-			console.log(commentElement);
 			document.body.appendChild(commentElement);	
 		});
 	});
