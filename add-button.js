0445f4b64f92f4aaa128574d9cb3610c403e54ca
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-01 11:52:21 -0400

    cannot remove modal

--- add-button.js
+++ add-button.js
@@ -5,44 +5,38 @@ $(document).ready(function(){
 		var $modal = $(modal);
 
 	(function() { 
-		var log = document.getElementsByClassName('whatever');
-		var theDiv = document.createElement('div');
-		theDiv.setAttribute('id', 'thediv');
-		theDiv.style.width = 100 + "%";
-		theDiv.style.position = 'Absolute';
-		theDiv.style.left = "0px";
-		theDiv.style.top = "0px";
-		$("body").prepend(theDiv);
 
+		var theDiv = document.getElementById('thediv');
 		var theButton = document.createElement('button');
 		theButton.setAttribute('class', 'whatever');
 
-		theButton.innerText = 'com';
 		theButton.style.position = 'relative';
 		theButton.style.left = (x - 20) +'px';
 		theButton.style.top = (y - 20) +'px';	
 		theButton.style.zIndex = "9997";
+		theButton.style.width = "2em";
+		theButton.style.height = "2em";
+
 		theButton.setAttribute('class', 'btn btn-danger-outline');
 		theButton.setAttribute('data-toggle', 'modal');
 		theButton.setAttribute('data-target', '#myModal');
 		$modal.appendTo(theDiv);
-
+		console.log(document.getElementById('thediv'));
 		document.getElementById('thediv').appendChild(theButton);
 
 				// Get the modal
 		var modal = document.getElementById('myModal');
 
-		// Get the button that opens the modal
-		var btn = document.getElementById("myBtn");
-
-		// Get the <span> element that closes the modal
-		var span = document.getElementsByClassName("close")[0];
-
 		// When the user clicks on the button, open the modal 
-		theButton.onclick = function() {
-		    modal.style.display = "block";
-		    modal.find("textarea").val() = "";
-		};
+		// theButton.onclick = function() {
+		//     modal.style.display = "block";
+		//     // modal.find("textarea").val() = "";
+		// };
+
+		$(theButton).on('click', function(){
+			console.log("hey");
+			$(this).prev().css("display", "block");
+		})
 
 		// When the user clicks anywhere outside of the modal, close it
 		window.onclick = function(event) {
@@ -55,7 +49,7 @@ $(document).ready(function(){
 			var comment = $(this).prev();
 			var newPar = $("<div class='alert alert-success'><p> user: " + comment.val() + "</p></div>");
 			$(this).parent().append(newPar);
-			modal.style.display = "none";
+			// $(this).parent().css("display", "none");;
 			console.log(theDiv);
 			chrome.runtime.sendMessage(theDiv);
 		});
--- content.js
+++ content.js
@@ -1,6 +1,13 @@
 
 
 $(document).ready(function(){
+	var theDiv = document.createElement('div');
+		theDiv.setAttribute('id', 'thediv');
+		theDiv.style.width = 100 + "%";
+		theDiv.style.position = 'Absolute';
+		theDiv.style.left = "0px";
+		theDiv.style.top = "0px";
+		$("body").prepend(theDiv);
 	$(document).on('contextmenu', function(event){
 			var x = event.clientX + document.body.scrollLeft;
 			var y = event.clientY + document.body.scrollTop;
