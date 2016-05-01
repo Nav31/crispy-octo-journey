7614e9427c202952d6afd1e3de388c52ff308d06
Massimo Crapanzano <massimo.crapanzano@gmail.com>
2016-05-01 12:41:05 -0400

    can create different button that work and update the right part of the dom

--- add-button.js
+++ add-button.js
@@ -1,59 +1,47 @@
 
 $(document).ready(function(){
-	// var modal = modal;
 
-		var $modal = $(modal);
+	var $modal = $(modal);
+	var currentModal;
 
-	(function() { 
+	var theDiv = document.getElementById('thediv');
+	var theButton = document.createElement('button');
+	theButton.setAttribute('class', 'whatever');
 
-		var theDiv = document.getElementById('thediv');
-		var theButton = document.createElement('button');
-		theButton.setAttribute('class', 'whatever');
+	theButton.style.position = 'relative';
+	theButton.style.left = (x - 20) +'px';
+	theButton.style.top = (y - 20) +'px';	
+	theButton.style.zIndex = "9997";
+	theButton.style.width = "2em";
+	theButton.style.height = "2em";
 
-		theButton.style.position = 'relative';
-		theButton.style.left = (x - 20) +'px';
-		theButton.style.top = (y - 20) +'px';	
-		theButton.style.zIndex = "9997";
-		theButton.style.width = "2em";
-		theButton.style.height = "2em";
+	theButton.setAttribute('class', 'btn btn-danger-outline');
+	theButton.setAttribute('data-toggle', 'modal');
+	theButton.setAttribute('data-target', '#myModal');
+	$modal.appendTo(theDiv);
+	console.log("TheDiv ", document.getElementById('thediv'));
+	document.getElementById('thediv').appendChild(theButton);
 
-		theButton.setAttribute('class', 'btn btn-danger-outline');
-		theButton.setAttribute('data-toggle', 'modal');
-		theButton.setAttribute('data-target', '#myModal');
-		$modal.appendTo(theDiv);
-		console.log(document.getElementById('thediv'));
-		document.getElementById('thediv').appendChild(theButton);
+			// Get the modal
+	// var modal = document.getElementById('myModal');
 
-				// Get the modal
-		var modal = document.getElementById('myModal');
-
-		// When the user clicks on the button, open the modal 
-		// theButton.onclick = function() {
-		//     modal.style.display = "block";
-		//     // modal.find("textarea").val() = "";
-		// };
-
-		$(theButton).on('click', function(){
-			console.log("hey");
-			$(this).prev().css("display", "block");
-		})
-
-		// When the user clicks anywhere outside of the modal, close it
-		window.onclick = function(event) {
-		    if (event.target == modal) {
-		        modal.style.display = "none";
-		    }
-		};
-
-		$("#postIt").on('click', function(){
+	$(theButton).on('click', function(){
+		currentModal = $(this).prev();
+		currentModal.css("display", "block");
+		currentModal.find("#postIt").on('click', function(){
 			var comment = $(this).prev();
 			var newPar = $("<div class='alert alert-success'><p> user: " + comment.val() + "</p></div>");
 			$(this).parent().append(newPar);
-			// $(this).parent().css("display", "none");;
+			console.log($(this).parent().parent());
+			currentModal.css("display", "none");
 			console.log(theDiv);
 			chrome.runtime.sendMessage(theDiv);
 		});
-	})();
+		currentModal.find("#closeIt").on('click', function(){
+			currentModal.css("display", "none");
+		});
+	});
+
 });
 
 
